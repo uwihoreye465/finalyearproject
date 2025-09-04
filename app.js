4f2bdbe23a4a113efc0e002a240a742423460c71
uwihoreye465 <Uwihoreyefrancois12@gmail>
2025-09-04 09:38:50 +0200

    gitignoreport

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:6000',
   credentials: true
 }));
 
